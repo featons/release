237b689bc5c1d2233e4c0ce85a833067bc6e7983
featon <48486614+featons@users.noreply.github.com>
2024-06-30 16:20:25 +0200

    Delete build directory

--- build/proc-macro2-867cad8ffbe5e3cd/build_script_build-867cad8ffbe5e3cd.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Mostafa\csv_splitter\target\release\build\proc-macro2-867cad8ffbe5e3cd\build_script_build-867cad8ffbe5e3cd.exe: C:\Users\Mostafa\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.86\build.rs
-
-C:\Users\Mostafa\csv_splitter\target\release\build\proc-macro2-867cad8ffbe5e3cd\build_script_build-867cad8ffbe5e3cd.d: C:\Users\Mostafa\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.86\build.rs
-
-C:\Users\Mostafa\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.86\build.rs:
--- build/serde-ae75484edb859e7c/build_script_build-ae75484edb859e7c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Mostafa\csv_splitter\target\release\build\serde-ae75484edb859e7c\build_script_build-ae75484edb859e7c.exe: C:\Users\Mostafa\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.203\build.rs
-
-C:\Users\Mostafa\csv_splitter\target\release\build\serde-ae75484edb859e7c\build_script_build-ae75484edb859e7c.d: C:\Users\Mostafa\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.203\build.rs
-
-C:\Users\Mostafa\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.203\build.rs:
